bdc7e49bbe6af9ca6fccf43655747ef2e3ab5e1c
s17a2048 <s17a2048@bene.fit.ac.jp>
2021-01-20 16:50:58 +0900

    1

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>テストwebアプリ</title>
+    </head>
+    <body>
+        <form action="index.php" method="post">
+            <br>id:<input type="text" name="id"><br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
